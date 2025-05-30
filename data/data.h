8dc628d356642bac1c61a6d348b6a687c2eb9b70
Luiz Felipe Brandao <luiz.brandao.2602@gmail.com>
2025-05-30 07:40:26 -0300

    correção de bugs e remoção do tipos.h

--- data/data.h
+++ data/data.h
@@ -1,10 +1,17 @@
 #ifndef DATA_H
 #define DATA_H
 
-#include "../tipos.h"
+// #include "../tipos.h"
+
+typedef struct{
+    int dia;
+    int mes;
+    int ano;
+} Data;
 
 // Protótipo das funções
 Data dataAtual();
-int calcIdade(Pessoa pessoas[], int n);
+
+int calcIdade(Data nascimento);
 
 #endif // DATA_H
